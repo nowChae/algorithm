d34773f797a772557b99615a960e5d80d2af5fe1
Chaewon Lee <lcw8447@naver.com>
2023-09-13 01:31:16 +0900

    programmers 전화번호 목록

--- programmers전화번호목록.py
+++ programmers전화번호목록.py
@@ -22,13 +22,23 @@ def solution(phone_book):
 
 def solution(phone_book):
     answer = True
+
+    #해시맵 설정
     hash_map = {}
+
+    #전화번호를 해시 맵에 저장 
     for phone_number in phone_book:
         hash_map[phone_number] = 1
+
+
     for phone_number in phone_book:
         temp = ""
         for number in phone_number:
-            temp += number
+            temp += number # 접두사를 저장 
+            #temp에 하나씩 숫자를 추가해나가면서 판단하는데,
+            #이 때 temp의 값이 hash_map (딕셔너리)에 키 값 중 있다면
+            #그리고 길이가 기존 문자열과 같지 않으면
+            #접두사가 되는 전화 번호가 있는 경우임
             if temp in hash_map and temp != phone_number:
                 answer = False
     return answer
