e3df1a2b869a5c1541b56d3946ffa2126ccb8a53
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-01 17:41:08 +0900

    [Bronze II] Title: 카이사르 암호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5598. 카이사르 암호/카이사르 암호.py
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.py
@@ -1,9 +1,8 @@
 string = input()
 
-result = []
+
 for s in string:
     r = ord(s) - 3
     if r < 65:
         r += 26    
-    result.append(chr(r))
-print(''.join(result))
+    print(chr(r),end='')
