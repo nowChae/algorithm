b432c54ad23a86a03a799625ccb45c3c187dd3d5
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-06 19:00:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+memo = [0 for _ in range(n+1)]
+
+for i in range(n+1):
+  if i==0:
+    memo[i] = 0
+  elif i==1:
+    memo[i] = 1
+  elif i==2:
+    memo[i] = 3
+  else:
+    memo[i] = memo[i-1]+2*memo[i-2]
+
+result = memo[n] % 10007
+print(result)
