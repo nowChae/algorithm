18ecde07fe35461b50ac88ea7e3312c45deb7126
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-08 23:51:53 +0900

    [level 2] Title: 주식가격, Time: 16.25 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,15 @@
-
 def solution(prices):
     n = len(prices)
-    stack = []
-    answer = [0] * n
-    
-    for i, price in enumerate(prices):
-        # 스택이 비어있지 않고 현재 주식 가격이 스택의 가장 위에 있는 가격보다 작을 경우
-        while stack and price < prices[stack[-1]]:
-            j = stack.pop()  # 스택에서 주식 가격을 꺼내옴
-            answer[j] = i - j  # 현재 시간에서 떨어지는 기간을 계산하여 저장
+    answer = [0 for _ in range(n)]
 
-        stack.append(i)  # 현재 시간을 스택에 추가
+    stack = []  # 현재 시점을 저장할 스택
+    for i in range(n):
+        # 스택이 비어있지 않고 현재 주식 가격이 스택의 마지막 가격보다 작거나 같다면
+        while stack and prices[i] < prices[stack[-1]]:
+            j = stack.pop()  # 스택의 마지막 값을 꺼내옴
+            answer[j] = i - j  # 현재 시점에서 떨어진 기간을 계산하여 저장
+
+        stack.append(i)  # 현재 시점을 스택에 추가
 
     # 스택에 남아 있는 주식 가격 처리
     for i in stack:
