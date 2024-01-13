3527ceee2d85181770ae9638291877d3d51bd0da
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-13 23:46:47 +0900

    [level Lv.3] Title: 네트워크, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
@@ -1,18 +1,42 @@
+from collections import deque
+
 def dfs(start, graph, visited, result): #재귀
     visited[start] = True
     result.append(start)
     for i in range(len(visited)):
         if (not visited[i]) and (graph[start][i]):
             dfs(i, graph, visited,result)
+            
+def bfs(start, graph, visited, result): #큐
+    queue = deque([start])
+    visited[start] = True
+    result.append(start)
+    
+    while queue:
+        v = queue.popleft()
+        result.append(v)
+        for i in range(len(visited)):
+            if (not visited[i]) and (graph[v][i]):
+                queue.append(i)
+                visited[i] = True
+                
     
 
 def solution(n, computers):
     visited = [False] * n
     answer = 0
+    """
     for i in range(n):
         result = []
         if not visited[i]: 
-            dfs(i, computers, visited,result)
+            dfs(i, computers, visited, result)
+        if result:
+            answer+= 1
+    """
+    for i in range(n):
+        result = []
+        if not visited[i]: 
+            bfs(i, computers, visited, result)
         if result:
             answer+= 1
     
