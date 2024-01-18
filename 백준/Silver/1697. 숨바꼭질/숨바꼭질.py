3e43540d6468175fc95cf218ec3cf85f2ea6f3ec
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-19 00:26:38 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,32 @@
+
+import sys
+from collections import deque
+
+N, K = map(int, sys.stdin.readline().split())
+
+visited = [False]*(100001)
+queue = deque([N, 0])
+visited[N] = True
+while True:
+  cur = queue.popleft()
+  sec = queue.popleft()
+
+  if cur == K:
+    print(sec)
+    break
+  else:
+    if cur - 1 >= 0:
+      if not visited[cur-1]:
+        queue.append(cur-1)
+        queue.append(sec+1)
+        visited[cur-1] = True
+    if cur + 1 <= 100000:
+      if not visited[cur+1]:
+        queue.append(cur+1)
+        queue.append(sec+1)
+        visited[cur+1] = True
+    if cur * 2 <= 100000:  
+      if not visited[cur*2]:
+        queue.append(cur*2)
+        queue.append(sec+1)
+        visited[cur*2] = True
