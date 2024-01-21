cd36e627d538feb1f327b34b5cdf09508ed4c5a5
nowChae <99425616+nowChae@users.noreply.github.com>
2024-01-21 21:21:41 +0900

    [level 2] Title: 타겟 넘버, Time: 1.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,14 @@
 def solution(numbers, target):
     answer = 0
-    tree = [0]
+    nodes = [0]
     for n in numbers:
         cur = []
-        for t in tree:
-            cur.append(t+n)
-            cur.append(t-n)
-        tree = cur
+        for d in nodes:
+            cur.append(d+n)
+            cur.append(d-n)
+        nodes = cur
         
-    for r in tree:
+    for r in nodes:
         if r == target:
             answer += 1
     return answer
