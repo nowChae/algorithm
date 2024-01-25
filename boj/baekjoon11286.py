a287d70d41b93b96a7bf11bbbd51c77b51903371
nowChae <nowChae23@gmail.com>
2024-01-25 18:53:51 +0900

    11286 졀댓값 힙 - heapq 모듈 사용

--- boj/baekjoon11286.py
+++ boj/baekjoon11286.py
@@ -3,18 +3,18 @@ import heapq
 input = sys.stdin.readline
 
 N = int(input())
-heap = []
+abs_heap = []
 
 for _ in range(N):
   i = int(input())
 
   if i == 0:
-    if len(heap) == 0:
+    if len(abs_heap) == 0:
       print(0)
     else:
-      print(heapq.heappop(heap)[1])
+      print(heapq.heappop(abs_heap)[1])
   else:
     if i > 0:
-      heapq.heappush(heap, (i,i))
+      heapq.heappush(abs_heap, (i,i))
     else:
-      heapq.heappush(heap, (-i,i))
+      heapq.heappush(abs_heap, (-i,i))
