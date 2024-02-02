6b6c5c68347d80bdc38763c1a8b79d0996cedd7c
nowChae <99425616+nowChae@users.noreply.github.com>
2024-02-02 16:24:14 +0900

    [Bronze II] Title: 소수, Time: 332 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+m = int(input())
+n = int(input())
+
+def prime_number(num):
+    if num == 1:
+        return False
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+        
+    return True
+
+
+min_prime = 0
+count = 0
+sum_prime = 0
+for i in range(m, n+1):
+    if prime_number(i):
+        if count == 0:
+            min_prime = i
+        sum_prime += i
+        count += 1
+
+if sum_prime == 0:
+    print(-1)
+else:
+    print(sum_prime)
+    print(min_prime)
