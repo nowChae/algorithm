4c3ab71cc1417c6c86615a03c25621a426dc7782
nowChae <nowchae23@gmail.com>
2024-02-07 16:30:33 +0900

    17266 어두운 굴다리 - 이분 탐색

--- boj/baekjoon17266-02.py
+++ boj/baekjoon17266-02.py
@@ -1,3 +1,8 @@
+#이분 탐색 사용
+#가로등의 높이를 이분 탐색으로 찾기
+#만약 binary 함수가 성공하면 가로등의 높이를 줄이는 방향으로 또 탐색, 가로등 높이 갱신 
+#실패하면 가로등의 높이를 키우는 방향으로
+
 import sys
 input= sys.stdin.readline
 
@@ -16,7 +21,6 @@ def binary(m_list, mid):
     for i in range(1, len(m_list)-2):
         if (m_list[i+1]-m_list[i])/2 > mid:
             return 0
-    
     return 1
 
 while start<=end:
