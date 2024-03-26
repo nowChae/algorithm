c8b0dde8e9593ba190f94458a5dfd0be912ea6d7
nowChae <99425616+nowChae@users.noreply.github.com>
2024-03-26 20:04:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(str,input().rstrip())))
+
+visited = [[False for _ in range(N)] for _ in range(N)]
+
+def dfs(start_i, start_j, count):
+    visited[start_i][start_j] = True
+    count += 1
+
+    if (start_j+1 != N) and (graph[start_i][start_j + 1]=='1') and (not visited[start_i][start_j + 1]):
+        count = dfs(start_i, start_j+1, count)
+    if (start_i+1 != N) and (graph[start_i + 1][start_j]=='1') and (not visited[start_i + 1][start_j]):
+        count = dfs(start_i+1, start_j, count)
+    if (start_j-1 != -1) and(graph[start_i][start_j - 1]=='1') and (not visited[start_i][start_j - 1]):
+        count = dfs(start_i, start_j-1, count)
+    if (start_i-1 != -1) and (graph[start_i-1][start_j]=='1') and (not visited[start_i - 1][start_j]):
+        count = dfs(start_i - 1, start_j, count)
+    
+    return count
+
+result = []
+for i in range(N):
+    for j in range(N):
+        if (graph[i][j] == '1') and (not visited[i][j]):
+            result.append(dfs(i,j,0))
+
+
+print(len(result))
+result.sort()
+for r in result:
+    print(r)
