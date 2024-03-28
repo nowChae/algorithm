4ffcbb5c6a4873ad02469a89b4a2699ef555ca06
nowChae <nowchae23@gmail.com>
2024-03-28 17:01:14 +0900

    17070 파이프 옮기기1 - dp

---
+++ boj/baekjoon17070-02.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+house = []
+for _ in range(N):
+    house.append(list(map(int,input().split())))
+
+def pipe_connect():
+    dp = [[[0] * 3 for _ in range(N)] for _ in range(N)]
+    dp[0][1][0] = 1
+    # 마지막 값 0: 가로, 1: 세로, 2: 대각선
+
+    for i in range(N):
+        for j in range(2, N):
+            if house[i][j] == 0:  # 현재 위치가 빈 공간일 때
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2]  # 가로 이동
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2]  # 세로 이동
+
+                if house[i - 1][j] == 0 and house[i][j - 1] == 0:  # 대각선 이동 가능한 경우
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2]
+
+    return sum(dp[N - 1][N - 1])
+
+print(pipe_connect())
