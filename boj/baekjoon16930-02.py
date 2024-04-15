5a775a79fbe4b8702232f626b328e80d555474a1
nowChae <nowchae23@gmail.com>
2024-04-15 17:28:55 +0900

    16930 달리기 - bfs

--- boj/baekjoon16930-02.py
+++ boj/baekjoon16930-02.py
@@ -18,21 +18,21 @@ def bfs():
     while queue:
         x, y = queue.popleft()
         
-        if x == x2-1 and y == y2-1:
-            return dist[x][y]
+        if x == x2-1 and y == y2-1: #목적지 도착
+            return dist[x][y] # 시간 리턴
         
-        for i in range(4):
-            for j in range(1, k + 1):
+        for i in range(4): # 방향
+            for j in range(1, k + 1): # 최대 거리 
                 nx = x + dx[i] * j
                 ny = y + dy[i] * j
                 
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                if nx < 0 or nx >= n or ny < 0 or ny >= m: # 범위 안이 아니면
                     break
-                if ground[nx][ny] == '#':
+                if ground[nx][ny] == '#':# 벽에 마주하면
                     break
-                if dist[nx][ny] != -1 and dist[nx][ny] <= dist[x][y]:
+                if dist[nx][ny] != -1 and dist[nx][ny] <= dist[x][y]: #이미 방문한 위치인데, 기록된 값이 현재와 같거나 작을 경우 더 탐색 X
                     break
-                if dist[nx][ny] == -1:
+                if dist[nx][ny] == -1: #아직 방문하지 않은 곳이면
                     queue.append((nx, ny))
                     dist[nx][ny] = dist[x][y] + 1
     
