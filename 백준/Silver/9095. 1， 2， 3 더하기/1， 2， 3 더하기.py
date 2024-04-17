afb5191438dd418f884a18f8480c08eb5259f71a
nowChae <99425616+nowChae@users.noreply.github.com>
2024-04-17 23:34:29 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+def test(k):
+    if k == 1:
+        return 1
+    elif k == 2:
+        return 2
+    elif k == 3:
+        return 4
+    else:
+        return test(k-3) + test(k-2) + test(k-1)
+    
+result = []
+
+for _ in range(n):
+    k = int(input())
+    result.append(test(k))
+
+for t in result:
+    print(t)
