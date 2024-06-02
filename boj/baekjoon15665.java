892fad495f6a0e5f58783de5d7918a42dd0dc4e9
nowChae <nowchae23@gmail.com>
2024-06-02 17:56:00 +0900

    11565 - java

---
+++ boj/baekjoon15665.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class baekjoon15665 {
+    static int N, M;
+    static int[] nList;
+    static List<Integer> result = new ArrayList<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer NM = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(NM.nextToken());
+        M = Integer.parseInt(NM.nextToken());
+
+        Set<Integer> nSet = new HashSet<>();
+        StringTokenizer numbers = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nSet.add(Integer.parseInt(numbers.nextToken()));
+        }
+
+        nList = new int[nSet.size()];
+        int index = 0;
+        for (int num : nSet) {
+            nList[index++] = num;
+        }
+
+        Arrays.sort(nList);
+
+        dfs();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void dfs() throws IOException {
+        if (result.size() == M) {
+            for (int num : result) {
+                bw.write(num + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = 0; i < nList.length; i++) {
+            result.add(nList[i]);
+            dfs();
+            result.remove(result.size() - 1);
+        }
+    }
+}
