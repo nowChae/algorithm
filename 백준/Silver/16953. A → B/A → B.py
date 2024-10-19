2ee9fa2d54da7e221de7600ec7671237d1c1e4b7
nowChae <99425616+nowChae@users.noreply.github.com>
2024-10-19 22:20:37 +0900

    [Silver II] Title: A → B, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,22 +1,21 @@
-#dfs
 import sys
 input = sys.stdin.readline
 
-A, B = map(int, input().split())
+A, B = map(int,input().split())
 
 result = []
-def dfs(start, end, count):
-    if start == end:
-        result.append(count)
-        return
-    elif start < end:
-        dfs(start*2, end, count+1)
-        dfs(start*10 + 1, end, count+1)
+def recursive(num, idx):
 
-    return 
-dfs(A, B, 1)
+    if num == B:
+        result.append(idx+1)
+    elif num > B:
+        return -1
+    recursive(num*10 + 1, idx+1)
+    recursive(num*2, idx+1)
 
-if len(result) == 0:
-    print(-1)
-else:
+recursive(A,0)
+
+if result: 
     print(min(result))
+else:
+    print(-1)
