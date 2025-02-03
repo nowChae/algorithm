29d17c2f402c3b87abd85f9a6fe71ebfd49766bf
nowChae <99425616+nowChae@users.noreply.github.com>
2025-02-03 11:53:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 132 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,20 +1,20 @@
 import sys
-import heapq
 input = sys.stdin.readline
+import heapq
 
 N = int(input())
+
 heap = []
 
 for _ in range(N):
-  i = int(input())
+    x = int(input())
 
-  if i == 0:
-    if len(heap) == 0:
-      print(0)
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            abs_min = heapq.heappop(heap)
+            print(abs_min[1])
+
     else:
-      print(heapq.heappop(heap)[1])
-  else:
-    if i > 0:
-      heapq.heappush(heap, (i,i))
-    else:
-      heapq.heappush(heap, (-i,i))
+        heapq.heappush(heap, (abs(x), x))
